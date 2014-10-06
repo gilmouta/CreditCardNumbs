e44a1564ad43e3bdbde1175e02543c1068e2ff56
mmcfus <gserpa@openmailbox.org>
2014-10-06 14:59:36 +0100

    code cleanup and comments added

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-#Vamos usar o slicing operator do python (':') para 'brincar' com as strings
+
 
 def luhn_verifica(x):
 	y = str(x)
 	z = str(x)
 	p = []
-	i = 0
 	j = 0
-	k = 0
 	soma = 0
+	
+	#Vamos usar o slicing operator do python (':') para 'brincar' com as strings
 	
 	#Tirar o último carácter
 	y = y[:-1]  #[:] vai buscar todos os characters na string excepto [-1] que é o último número
@@ -15,25 +15,26 @@ def luhn_verifica(x):
 	#Inverter o número
 	y = y[::-1]
 	
-	for i in range(len(y)):
+	#Converto a string y para uma array de integers em p[]
+	for i in range(len(y)): 
 		n = int(y[i])
 		p.append(n)
 	
 	while j < len(p):
-		if j % 2 == 0:
+		if j % 2 == 0: #Multiplico os integers nos indexes ímpares por 2
 			p[j] = p[j] * 2
-			if p[j] > 9:
+			if p[j] > 9: #Se um integer for maior do que 9, subtraio 9 desse integer
 				p[j] = p[j] - 9
 		j = j + 1
 		
-	for k in p:
+	for k in p: #Encontrar a soma de todos os números da array
 		soma += k	
 	soma = soma + eval(z[-1])
 	
-	if soma % 10 == 0:
-		print("Coolio")
-	else:
-		print("...what")
+	if soma % 10 == 0: #Se for divisivel por 10 então o número funciona de acordo com Luhn
+		print("Funciona")
+	else:			#Vai ser substituido por um return de uma boolean mais tarde
+		print("Não funciona")
 	
 	
 #Temporário, esta linha vai ser removida, está aqui só para os testes serem mais rápidos.	
