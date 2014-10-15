8820bc0f93e0cabd812813a60b8f446ccf4df6cf
mmcfus <gserpa@openmailbox.org>
2014-10-15 16:08:18 +0100

    finito

--- main.py
+++ main.py
@@ -43,46 +43,46 @@ def comeca_por(cad1, cad2):
 def comeca_por_um(cad, t_cads):
 	'''Recebe string (arg1) e lista de strings (arg2), devolve boolean. \n Devolve True se o primeiro argumento comecar por pelo menos uma string do segundo argumento.'''
 	i = 0
-	while i < len(t_cads):
-		if comeca_por(cad, t_cads[i]) == True:
+	while i < len(t_cads):  # Vai de string em string da lista
+		if comeca_por(cad, t_cads[i]) == True: # Se comecar por uma delas, devolve true
 			return True
 		else:
 			i += 1
 	return False
 
 def valida_iin(x):
-	'''Recebe string, devolve string. \n Devolve o nome da rede correspondente ao numero de cartao.'''
+	'''Recebe string, devolve string. \n Devolve o nome da rede correspondente ao numero de cartao se o tamanho estiver certo e comecar por um prefixo valido. Caso contrario devolve "".'''
 	x = str(x)
 	i, j = 0, 0
-	while not(comeca_por_um(x, listarede[i][2])):
-		i += 1
+	while not(comeca_por_um(x, listarede[i][2])): # Enquanto nao comecar por um prefixo valido...
+		i += 1 # Vai de rede em rede
 		j = i
-		if j == len(listarede):
+		if j == len(listarede):  # Se chegou ao fim devolve ""
 			return ""
-	if not(len(x) in listarede[j][1]):
+	if not(len(x) in listarede[j][1]): # Se o tamanho nao corresponde ao prefixo, devolve ""
 		return ""
-	return listarede[j][3]		
+	return listarede[j][3] # Devolve rede emissora
 
 def categoria(x):
 	'''Recebe string, devolve string. \n Devolve a categoria do emissor correspondente ao numero de cartao.'''
 	x = str(x)
-	y = eval(x[0])
-	return listacategoria[y-1]
+	y = eval(x[0]) # Poe primeiro caracter do string x no y
+	return listacategoria[y-1] # Devolve categoria correspondente a y
 
 def verifica_cc(x):
 	'''Recebe inteiro, devolve lista de strings. \n Devolve a categoria da rede emissora e o nome da rede emissora se o numero de cartao for valido. Senao devolve "numero invalido"'''
 	x = str(x)
-	if luhn_verifica(x) and valida_iin(x) != "":
+	if luhn_verifica(x) and valida_iin(x) != "": 
 		return (categoria(x), valida_iin(x))
 	else:
-		return "numero invalido"
+		return "cartao invalido"
 	
 def digito_verificacao(x):
 	'''Recebe string, devolve string. \n Devolve o digito final de um numero de cartao de forma a que verifique o algoritmo de Luhn.'''
 	s = str(x)
 	soma = calc_soma(s)
 	if soma%10 == 0:
-		digito = "0" 		#Se o ultimo digito for 0, devolve "0"
+		digito = "0"  #Se o ultimo digito for 0, devolve "0"
 	else:
 		digito = str(10-(soma % 10)) #Se nao, calcula fazendo 10 - ultimo digito
 	return digito
@@ -91,19 +91,19 @@ def gera_num_cc(rede):
 	'''Recebe string, devolve inteiro. \n Gera um numero de cartao da rede emissora dada.'''
 	i, j = 0, -1
 	while i < len(listarede):
-		if listarede[i][0] == rede:  	#Se a rede esta na lista de redes
-			j = i   		#j = indice abreviatura
+		if listarede[i][0] == rede:  #Se a rede esta na lista de redes
+			j = i #j = indice abreviatura
 		i += 1
-	if j == -1:  			#Se rede for invalida (nao encontrou abreviatura)		
+	if j == -1:  #Se rede for invalida (nao encontrou abreviatura)		
 		return "Rede invalida"
 
-	length = random.choice(listarede[j][1])
-	prefixo = random.choice(listarede[j][2])
+	length = random.choice(listarede[j][1]) # Escolhe aleatoriamente um length dos validos
+	prefixo = random.choice(listarede[j][2]) # Escolhe aleatoriamente um prefixo dos validos
 	nmeio= ""
 
-	while len(nmeio) != length-len(prefixo)-1:		#Enquanto houverem menos numeros que os necessarios -1
-		nmeio = nmeio + str(random.randint(0, 9))	#Ir adicionando numeros de 0 a 9
-	nfim = digito_verificacao(prefixo+nmeio)  	#Adicionar numero de verificacao
+	while len(nmeio) != length-len(prefixo)-1: #Enquanto houverem menos numeros que os necessarios -1
+		nmeio = nmeio + str(random.randint(0, 9)) #Ir adicionando numeros de 0 a 9
+	nfim = digito_verificacao(prefixo+nmeio)  #Adicionar numero de verificacao
 	numerocc = eval(prefixo+nmeio+nfim)
 
 	return numerocc
